b8ca7d3f4e00d72bbc4370f2b32b058874c3212a
Kevin Ansfield <kevin@lookingsideways.co.uk>
2010-10-28 14:37:29 +0100

    fix railtie_name deprecation

--- lib/acts_as_sanitiled/railtie.rb
+++ lib/acts_as_sanitiled/railtie.rb
@@ -2,9 +2,7 @@ require 'acts_as_sanitiled'
 require 'rails'
 
 module ActsAsSanitiled
-  class Railtie < Rails::Railtie
-    railtie_name :acts_as_sanitiled
-    
+  class Railtie < Rails::Railtie    
     initializer "acts_as_sanitiled.configure_rails_initialization" do
       ActiveRecord::Base.send(:include, ActsAsSanitiled)
     end
